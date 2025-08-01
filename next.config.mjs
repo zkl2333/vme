cf65bc66af962c124c57cd30cb33981f838841fe
zkl <i@zkl2333.com>
2025-08-01 10:33:47 +0800

    feat: 🎸 移除MDX相关配置和组件

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,6 @@
-import nextMDX from '@next/mdx'
-
-import { recmaPlugins } from './mdx/recma.mjs'
-import { rehypePlugins } from './mdx/rehype.mjs'
-import { remarkPlugins } from './mdx/remark.mjs'
-
-const withMDX = nextMDX({
-  extension: /\.mdx?$/,
-  options: {
-    remarkPlugins,
-    rehypePlugins,
-    recmaPlugins,
-  },
-})
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
   images: {
     remotePatterns: [
       {
